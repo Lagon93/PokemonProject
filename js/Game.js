88ca6fdab9fd88fcdbf786574fe59849018e1fed
Lagon93 <teixgacia@gmail.com>
2022-06-02 11:56:46 +0200

    test vue

--- js/Game.js
+++ js/Game.js
@@ -239,4 +239,13 @@ function ChangeMachine(newType) {
 function backgrounds(){
 	document.querySelectorAll('body').style = "background-image: url('../resources/arena.png');";
 }
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
 
