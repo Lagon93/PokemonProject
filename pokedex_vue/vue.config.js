bbdb54d1d26c84fced4fe6f9d282fd5c9e7aab36
Lagon93 <teixgacia@gmail.com>
2022-06-09 09:37:50 +0200

    otro commit probando vue2

--- pokedex_vue/vue.config.js
+++ pokedex_vue/vue.config.js
@@ -1,9 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.pokemonproject + '/'
-    : '/'
-}
+import{defineConfig}from'vite'
+import vue from'@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+ base:'/del
+  plugins:[vue()]
